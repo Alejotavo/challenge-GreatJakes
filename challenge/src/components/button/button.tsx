e66b3450c1b8e6cc1cdca675043083b18aceaf65
Alejo <alejogustavof@gmail.com>
2024-10-06 12:36:22 -0300

    refactor scss

--- challenge/src/components/button/button.tsx
+++ challenge/src/components/button/button.tsx
@@ -1,20 +1,21 @@
 import  { FC, ReactNode } from 'react';
-import { Button } from 'react-bootstrap';
 import styles from './button.module.scss';
 
 interface MainButtonProps {
-  variant: 'primary' | 'secondary' | 'disable';
+  variant: 'primary' | 'disable';
   children: ReactNode;
 }
 
 const MainButton: FC<MainButtonProps> = ({ variant, children }) => {
     return (
-      <Button className={`${styles.button} ${styles[variant]}`}>
-        <div className='btn-data-content'>
-        <i className="bi bi-search"></i> 
-        <div className='truncated-text'>{children}</div>
-        </div>
-      </Button>
+      <button className={`${styles.button} ${styles[variant]}`}>
+        <div className={styles['main-btn-content']}>
+            <div className={styles['btn-data-content']}>
+              <i className="bi bi-search"></i> 
+              <div className={styles['text']}>{children}</div>
+            </div>
+          </div>
+      </button>
     );
   };
   
--- challenge/src/components/modal/modal.tsx
+++ challenge/src/components/modal/modal.tsx
@@ -13,13 +13,13 @@ function ModalComponent() {
           </Modal.Header>
           <Modal.Body>
           <div className="grid-container">
-            <div className="grid-item grid-item-tall  btn-content white"><MainButton variant="primary">This is a two lines button that terminates with </MainButton></div>
-            <div className="grid-item btn-content green"> <MainButton variant="secondary">This is a two lines button that terminates with </MainButton></div>
-            <div className="grid-item btn-content display-icon white"> <MainButton variant="primary">This is a one line button</MainButton></div>
-            <div className="grid-item btn-content white"> <MainButton variant="primary">This is a two lines button that terminates with  </MainButton></div>
-            <div className="grid-item-no-flex  btn-content white pt-5"> <MainButton variant="primary">This is a two lines button that terminates with </MainButton></div>
-            <div className="grid-item-no-flex  btn-content white pt-5"> <MainButton variant="disable">This is a two lines button that terminates with </MainButton></div>
-            <div className="grid-item-no-flex  btn-content white pt-5"> <MainButton variant="primary">This is a two lines button that terminates with </MainButton></div>
+            <div className=" grid-item-tall"><MainButton variant="primary">This is a two lines button that terminates with </MainButton></div>
+            <div className="grid-item"> <MainButton variant="primary">This is a two lines button that terminates with </MainButton></div>
+            <div className="grid-item display-icon"> <MainButton variant="primary">This is a one line button</MainButton></div>
+            <div className="grid-item"> <MainButton variant="primary">This is a two lines button that terminates with  </MainButton></div>
+            <div className="grid-item"> <MainButton variant="primary">This is a two lines button that terminates with </MainButton></div>
+            <div className="grid-item"> <MainButton variant="disable">This is a two lines button that terminates with </MainButton></div>
+            <div className="grid-item"> <MainButton variant="primary">This is a two lines button that terminates with </MainButton></div>
         </div>
           </Modal.Body>
         </Modal.Dialog>
